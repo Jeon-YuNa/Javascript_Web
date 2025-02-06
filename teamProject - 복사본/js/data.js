f3d7feda8f9a035fcff8d8b5cbdf14f430d7d544
pjt <swedf123@gmail.com>
2025-02-06 17:03:31 +0900

    teamProject

--- teamProject - 복사본/js/data.js
+++ teamProject - 복사본/js/data.js
@@ -1,10 +1,81 @@
-export data = [
-  {
-    company: "진에어", //항공편
-    DepartureT: "9:45", //출발시간
-    ArrivalTime: "12:30", //도착시간
-    price: 159000, //가격
-    date: "2025-03-15", // 비행날짜
-    destination: "오사카", //도착지
-  },
+//항공사 랜덤
+const companyList = [
+  "진에어",
+  "대한항공",
+  "아시아나항공",
+  "티웨이항공",
+  "에어서울",
+  "에어부산",
+  "에어로케이",
 ];
+const companyRandom = () =>
+  companyList[Math.floor(Math.random() * companyList.length)];
+
+//여행지 랜덤
+const destList = [
+  { Ko: "도쿄", En: "TYO" },
+  { Ko: "오사카", En: "OSA" },
+  { Ko: "후쿠오카", En: "HUK" },
+  { Ko: "삿포로", En: "SAT" },
+  { Ko: "오이타", En: "OIT" },
+  { Ko: "가고시마", En: "KAG" },
+];
+const destRandom = () => destList[Math.floor(Math.random() * destList.length)];
+
+//가격 랜덤
+const randomPrice = () => Math.floor(Math.random() * 100 + 160) + "000";
+
+//24시간 변경
+const getHour = (x) => {
+  if (x < 10) return "0" + x;
+  else if (x == 24) return "00";
+  else return x;
+};
+//60분 변경
+const getMinute = (x) => {
+  if (x < 10) return "0" + x;
+  else if (x == 60) return "00";
+  else return x;
+};
+//도착시간
+const departureTime = () => {
+  const Hour = getHour(Math.floor(Math.random() * 25));
+  const Min = getMinute(Math.floor(Math.random() * 61));
+  return [Hour, Min];
+};
+const getMonth = (min, max) => {
+  const minMonth = Math.ceil(min);
+  const maxMonth = Math.floor(max);
+  return Math.floor(Math.random() * (maxMonth - minMonth + 1) + minMonth);
+};
+const getDay = (min, max) => {
+  const minDay = Math.ceil(min);
+  const maxDay = Math.floor(max);
+  return Math.floor(Math.random() * (maxDay - minDay + 1) + minDay);
+};
+const MonthDay = () => {
+  const month = getHour(getMonth(1, 6));
+  const day = getMinute(getDay(1, 31));
+  if (month % 2 ? day <= 31 : day <= 30) {
+  } else if (month == 2) {
+    day <= 28;
+  }
+  return [month, day];
+};
+
+export const data = Array(6000)
+  .fill(0)
+  .map(() => {
+    const [hour, min] = departureTime();
+    const { Ko, En } = destRandom();
+    const [month, day] = MonthDay();
+    return {
+      company: companyRandom(), // 항공사
+      price: randomPrice(), // 가격
+      departureTime: `${hour}:${min}`, //출발시간
+      arrivalTime: `${getHour(+hour + 2)}:${min}`, //도착시간
+      destination: Ko, // 여행지
+      air: En,
+      date: `2025-${month}-${day}`,
+    };
+  });
--- teamProject - 복사본/js/search.js
+++ teamProject - 복사본/js/search.js
@@ -1,24 +1,12 @@
+import { data } from "./data.js";
+
 const arrive = document.querySelector(".arrive");
 const travel = document.querySelector("#travel");
 const travelList = document.querySelector("#travelList");
-const travelBtn = document.querySelectorAll("#travelList li");
-const departEn = document.querySelector(".depart strong");
-const departKo = document.querySelector(".depart span");
 const arriveEn = document.querySelector("#arriveEn");
 const arriveKo = document.querySelector("#arriveKo");
 const peoplePlus = document.querySelectorAll(".plus");
 const peopleMinus = document.querySelectorAll(".minus");
-
-const travelData = [
-  {
-    country: "오사카",
-    air: "OSA",
-  },
-  {
-    country: "도쿄",
-    air: "TYO",
-  },
-];
 
 // 여행할 지역창 보여주기
 arrive.addEventListener("click", () => {
@@ -26,18 +14,34 @@ arrive.addEventListener("click", () => {
 });
 
 // 여행할 지역 선택
-travelData.forEach((v) => {
+const a = data.map(({ destination, air }) => ({
+  destination,
+  air,
+}));
+const travelListBtn = [...new Set(a.map((v) => JSON.stringify(v)))].map((v) =>
+  JSON.parse(v)
+);
+
+// [
+//   ...new Set(
+//     data.map((v) => {
+//       const { destination, air } = v;
+//       return { destination, air };
+//     })
+//   ),
+// ]
+travelListBtn.forEach((v) => {
   //여행할 지역 리스트화
   const list = document.createElement("li");
   travelList.appendChild(list);
-  list.innerHTML = v.country;
+  list.innerHTML = v.destination;
 
   //클릭했을때 데이터값 HTML에넣어줌
   list.addEventListener("click", () => {
     travel.style.setProperty("display", "none");
 
-    arriveKo.innerHTML = v.country;
-    travelData.country == v.innerHTML
+    arriveKo.innerHTML = v.destination;
+    travelListBtn.destination == v.innerHTML
       ? (arriveEn.innerHTML = v.air)
       : (arriveEn.innerHTML = "");
   });
